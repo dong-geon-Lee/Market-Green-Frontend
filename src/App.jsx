b772483b13fd669454a77c2fe880bb2041089e85
dong-geon-Lee <ch9716@naver.com>
2022-04-17 14:58:04 +0900

    initial setting

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Register from "./pages/Register";
+import Login from "./pages/Login";
 
 const App = () => {
-  return <h1>Heelo world!! take over start!</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route index path="/" element={<Home></Home>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
