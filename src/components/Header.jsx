36795a7d70011c7e6a1d4fb2930ec519224fc184
dong-geon-Lee <ch9716@naver.com>
2022-06-17 14:48:54 +0900

    navbar media-query hover effect completed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -24,6 +24,7 @@ import { SiWebhint } from "react-icons/si";
 import { FaEnvira } from "react-icons/fa";
 import { FaPhotoVideo } from "react-icons/fa";
 import { BsFillImageFill } from "react-icons/bs";
+import { Mobile } from "../responsive";
 
 export const Container = styled.nav`
   display: flex;
@@ -41,6 +42,20 @@ export const Container = styled.nav`
   & a {
     text-decoration: none;
     color: #212529;
+
+    ${Mobile({
+      color: "#40c057",
+    })}
+
+    & svg {
+      ${Mobile({
+        color: "#343a40",
+      })}
+    }
+
+    &:hover {
+      transform: scale(1.05);
+    }
   }
 `;
 
@@ -80,7 +95,7 @@ export const Items = styled.li`
 export const ItemLink = styled.a`
   line-height: 1.5;
   color: #333;
-  font-size: 1.8rem;
+  font-size: 1.6rem;
   font-weight: 500;
   display: inline-block;
 `;
@@ -90,8 +105,8 @@ export const Button = styled.button`
   background-color: transparent;
   color: black;
   font-family: inherit;
-  font-size: 1.8rem;
-  font-weight: 500;
+  font-size: 1.6rem;
+  font-weight: 400;
   padding: 1.4rem 1.2rem;
   border: none;
   border-radius: 9px;
@@ -112,12 +127,32 @@ export const Button = styled.button`
     );
     background-position: 100%;
     background-color: #40c057;
+
+    ${Mobile({
+      color: "#fff",
+      marginLeft: "2rem",
+    })}
+
+    transform: scale(1.05);
+  }
+
+  ${Mobile({
+    fontSize: "3rem",
+    letterSpacing: "2px",
+    color: "#40c057",
+  })}
+
+  & svg {
+    ${Mobile({
+      color: "#343a40",
+    })}
   }
 `;
 
 export const BtnBox = styled.div`
   width: 100%;
   height: 100%;
+  display: block;
 
   &:last-child button {
     margin-left: 1.2rem;
@@ -138,6 +173,21 @@ export const CartBox = styled.div`
     width: 2.4rem;
     height: 2.4rem;
     fill: black;
+
+    ${Mobile({
+      width: "3.2rem",
+      height: "3.2rem",
+    })}
+  }
+
+  &:hover {
+    ${Mobile({
+      marginLeft: "2rem",
+    })}
+  }
+
+  &:hover {
+    transform: scale(1.05);
   }
 `;
 
@@ -147,19 +197,36 @@ export const CartText = styled.span`
   font-size: 1.5rem;
   position: absolute;
   top: 25%;
-  left: 42%;
+  left: 43%;
   transform: translate(-50%, -50%);
   background-color: beige;
   padding: 0.5rem 0.6rem;
   border-radius: 50%;
+
+  ${Mobile({
+    fontSize: "2.8rem",
+    left: "44.7%",
+  })}
 `;
 
 export const CartSubText = styled.p`
   color: black;
-  font-size: 1.8rem;
-  font-weight: 500;
+  font-size: 1.6rem;
+  font-weight: 400;
   line-height: 1.5;
   margin-left: 0.5rem;
+
+  ${Mobile({
+    fontSize: "3rem",
+    letterSpacing: "2px",
+    color: "#40c057",
+  })}
+
+  &:hover {
+    ${Mobile({
+      color: "#fff",
+    })}
+  }
 `;
 
 export const Navbar = styled.div`
@@ -181,7 +248,7 @@ export const LinkTag = styled(Link)`
 export const LinkStyle = styled(Link)`
   width: 100%;
   color: #333;
-  font-size: 1.8rem;
+  font-size: 1.6rem;
   font-weight: 500;
   line-height: 1.5;
   display: flex;
@@ -189,7 +256,6 @@ export const LinkStyle = styled(Link)`
   justify-content: center;
   border-radius: 9px;
   padding: 1.4rem 0;
-
   transition: all 0.25s cubic-bezier(0.68, 0.1, 0.75, 1.5);
   text-transform: uppercase;
   background-size: 220%;
@@ -203,22 +269,33 @@ export const LinkStyle = styled(Link)`
     );
     background-position: 100%;
     background-color: #40c057;
+  }
+
+  ${Mobile({
+    fontSize: "3rem",
+    letterSpacing: "1.5px",
+  })}
+
+  &:hover {
+    ${Mobile({
+      color: "#fff",
+      marginLeft: "2rem",
+    })}
   }
 `;
 
 export const NavText = styled.a`
   color: #333;
-  font-size: 1.8rem;
+  font-size: 1.6rem;
   font-weight: 500;
   display: flex;
   align-items: center;
-  line-height: 1.5;
+  line-height: 1.3;
   cursor: pointer;
   padding: 1.4rem 0;
   width: 100%;
   border-radius: 9px;
   justify-content: center;
-
   transition: all 0.25s cubic-bezier(0.68, 0.1, 0.75, 1.5);
   text-transform: uppercase;
   background-size: 220%;
@@ -232,7 +309,17 @@ export const NavText = styled.a`
     );
     background-position: 100%;
     background-color: #40c057;
+
+    ${Mobile({
+      color: "#fff",
+      marginLeft: "2rem",
+    })};
   }
+
+  ${Mobile({
+    fontSize: "3rem",
+    letterSpacing: "1.5px",
+  })}
 `;
 
 export const Nav = styled.nav`
@@ -242,23 +329,44 @@ export const Nav = styled.nav`
   justify-content: center;
   position: fixed;
   top: 0;
-  right: ${(props) => (props.active ? "0%" : "-100%")};
+  right: ${(props) => (props.active ? "0%" : "-110%")};
   transition: ${(props) => (props.active ? "0.35s" : "0.85s")};
   z-index: 10;
+  overflow: hidden;
+
+  ${Mobile({
+    width: "100vw",
+  })}
 `;
 
 export const Side = styled.ul`
   width: 100%;
   background-color: #ebfbee;
+
+  ${Mobile({
+    display: "flex",
+    flexDirection: "column",
+    width: "100vw",
+    height: "100vh",
+    alignItems: "center",
+    justifyContent: "center",
+    overflow: "hidden",
+  })}
 `;
 
 export const Toggle = styled.li`
   width: 100%;
-  height: 8rem;
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 3rem;
+  margin: 2rem 0 1rem 0;
+  transition: all 0.3s ease;
+
+  & svg:hover {
+    color: red;
+    transform: scale(1.03) translateY(0.1rem);
+  }
 `;
 
 export const Content = styled.div`
@@ -275,7 +383,6 @@ export const AdminBox = styled.div`
 
   & .cartLink {
     padding: 1.4rem 0;
-
     transition: all 0.25s cubic-bezier(0.68, 0.1, 0.75, 1.5);
     text-transform: uppercase;
     background-size: 220%;
@@ -291,6 +398,20 @@ export const AdminBox = styled.div`
       background-color: #40c057;
     }
   }
+
+  & .profile-svg {
+    & svg {
+      font-size: 1.8rem;
+
+      ${Mobile({
+        fontSize: "3rem",
+      })}
+    }
+  }
+
+  ${Mobile({
+    width: "100vw",
+  })}
 `;
 
 const Header = () => {
@@ -383,9 +504,12 @@ const Header = () => {
                         Admin
                       </Button>
                     )}
-                    <Button onClick={() => profileLink()}>
+                    <Button
+                      onClick={() => profileLink()}
+                      className="profile-svg"
+                    >
                       <BsFillFileEarmarkPersonFill
-                        style={{ marginRight: "1rem", fontSize: "2.3rem" }}
+                        style={{ marginRight: "1rem" }}
                       />
                       Profile
                     </Button>
